54447c095d5a8d638c3a7c2c505c747792b69a49
E. Choroba <choroba@matfyz.cz>
2024-10-17 14:08:42 +0200

    Fix more URLs
    - Use index.pl everywhere to workaround a PM misconfiguration;
    - Use the browse_url instead of a hardcoded path.

--- lib/PM/CB/GUI.pm
+++ lib/PM/CB/GUI.pm
@@ -30,7 +30,7 @@ sub url {
     my ($self, $url, $part) = @_;
     $url //= '__PM_CB_URL__';
     $part //= "?node=";
-    $url =~ s{__PM_CB_URL__}{https://$self->{browse_url}/$part};
+    $url =~ s{__PM_CB_URL__}{https://$self->{browse_url}/index.pl$part};
     return $url
 }
 
@@ -470,7 +470,8 @@ sub show_title {
 
 sub show_shortcut {
     my ($self, $shortcut, $url, $title) = @_;
-    $url = "https://www.perlmonks.org/?node=$shortcut" if 0 == length $url;
+    $url = "https://$self->{browse_url}/index.pl?node=$shortcut"
+        if 0 == length $url;
     delete $self->{shortcuts}{$shortcut};
     my $old_tag = "shortcut:$shortcut|$title";
     my $new_tag = "browse:$url|$title";
